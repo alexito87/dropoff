2e597d668b415b0d5c87cf882ac42c280873ff2c
Alex <a.ruzhantsou@gmail.com>
2026-05-14 14:24:22 +0300

    Refactor moderation into items module

--- backend/app/modules/items/index.py
+++ backend/app/modules/items/index.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.modules.items.routes.items_routes import router as items_routes
+from app.modules.items.routes.moderation_routes import router as moderation_routes
 
 router = APIRouter()
 
@@ -8,4 +9,10 @@ router.include_router(
     items_routes,
     prefix="/items",
     tags=["items"],
+)
+
+router.include_router(
+    moderation_routes,
+    prefix="/moderation",
+    tags=["moderation"],
 )
