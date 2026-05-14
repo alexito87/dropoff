e6eff18d681df10ec67e7e6171ccae9f3ab6c1aa
Alex <a.ruzhantsou@gmail.com>
2026-05-14 14:13:53 +0300

    Refactor cart into orders module

--- backend/app/modules/orders/index.py
+++ backend/app/modules/orders/index.py
@@ -1,8 +1,15 @@
 from fastapi import APIRouter
 
+from app.modules.orders.routes.cart_routes import router as cart_routes
 from app.modules.orders.routes.orders_routes import router as orders_routes
 
 router = APIRouter()
+
+router.include_router(
+    cart_routes,
+    prefix="/cart",
+    tags=["cart"],
+)
 
 router.include_router(
     orders_routes,
